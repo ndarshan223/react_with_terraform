838c1c9980e8e9ca7e447cdd721ac839177d0292
Darshan <ndarshan223@gmail.com>
2024-02-07 20:56:38 +0530

    adding custom details

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,8 @@ FROM node:14
 WORKDIR /app
 COPY package*.json ./
 RUN npm install --production 
+RUN npm install -g serve
 COPY . .
-EXPOSE 8080
-CMD ["npm","start"]
+RUN npm run build
+EXPOSE 5000
+CMD ["serve", "-s", "build", "-l", "5000"]
--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ function App() {
       <header className="App-header">
         <h1> Welcome to 2024 </h1>
 
+        <t1> Application built and deployed by Darshan R Naik</t1>
+        <t1>Mail id : ndarshan223@gmail.com</t1>
+
 
       </header>
     </div>
