417628c324c46bb8a3a9b34f2e6b84b62a70c375
Darshan <ndarshan223@gmail.com>
2024-02-08 03:21:37 +0530

    added links

--- src/App.js
+++ src/App.js
@@ -8,6 +8,14 @@ function App() {
 
         <t1> Application built and deployed by Darshan R Naik</t1>
         <t1>Mail id : ndarshan223@gmail.com</t1>
+        <h3> Repo code https://github.com/ndarshan223/assignment</h3>
+        
+        <button style={{ fontSize: '20px', padding: '10px 20px' }} onClick={() => window.open("https://github.com/ndarshan223/assignment/blob/main/terraform/ec2.tf", "_blank")}>Infrastructure Code</button> 
+        <br></br>        
+
+        <button style={{ fontSize: '20px', padding: '10px 20px' }} onClick={() => window.open("https://github.com/ndarshan223/assignment", "_blank")}>Application Code</button> 
+        <br></br>
+        <button style={{ fontSize: '20px', padding: '10px 20px' }} onClick={() => window.open("https://github.com/ndarshan223/assignment/blob/main/.github/workflows/cicd.yml", "_blank")}>Configuration file for CICD of Github workflow Self hosted runner</button>
 
 
       </header>
